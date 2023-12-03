a56e4c205e17194fe650ff7ee840951b5a406af7
Kevin Grimaldi <anthgrim@icloud.com>
2023-12-03 17:11:40 -0500

    adding folder structure

---
+++ basics/FirstProgram/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int favorite_number;
+
+    cout << "Enter your favorite number between 1 and 100: ";
+    cin >> favorite_number;
+    cout << "Your favorite number is: " << favorite_number << endl;
+
+    return 0;
+}
