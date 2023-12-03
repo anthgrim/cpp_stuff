03d7fe5778c11938a87dd1347338509a8a8dda4d
Kevin Grimaldi <anthgrim@icloud.com>
2023-12-03 16:35:32 -0500

    make it fun

--- basics/factorial.cpp
+++ basics/factorial.cpp
@@ -1,16 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int n = 2;
+int get_factorial (int input) {
     int factorial = 1;
-
     int i = 1;
-    while (i <= n) {
+
+    while (i <= input) {
         factorial = factorial * i;
         i++;
     }
 
-    cout << "The factorial of " << n << " is " << factorial << endl;
+    return factorial;
+}
+
+int main() {
+    int n = 5;
+    int factorial_of_number = get_factorial(n);
+
+    cout << "The factorial of " << n << " is " << factorial_of_number << endl;
     return 0;
 }
