0b45e0ca1349d5e1323f8c146acfda1f2377a994
Kevin Grimaldi <anthgrim@icloud.com>
2023-12-03 16:45:55 -0500

    Adding function documentation

--- basics/factorial.cpp
+++ basics/factorial.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * Get factorial of a number
+ * 
+ * @param int input
+ * @return factorial product
+*/
 int get_factorial (int input) {
     int factorial = 1;
     int i = 1;
